406cb338319a04bd18844aa567e208eab208d017
tcorral <amischol@gmail.com>
2014-03-28 18:12:32 +0100

    remove unneeded code in tests.

--- test/auto_package_test.js
+++ test/auto_package_test.js
@@ -2,28 +2,8 @@
 
 var grunt = require('grunt');
 var path = require('path');
-var fs = require('fs-extra');
 var cwd = process.cwd();
 
-/*
- ======== A Handy Little Nodeunit Reference ========
- https://github.com/caolan/nodeunit
-
- Test methods:
- test.expect(numAssertions)
- test.done()
- Test assertions:
- test.ok(value, [message])
- test.equal(actual, expected, [message])
- test.notEqual(actual, expected, [message])
- test.deepEqual(actual, expected, [message])
- test.notDeepEqual(actual, expected, [message])
- test.strictEqual(actual, expected, [message])
- test.notStrictEqual(actual, expected, [message])
- test.throws(block, [error], [message])
- test.doesNotThrow(block, [error], [message])
- test.ifError(value)
- */
 
 exports.auto_package = {
   setUp: function(done) {
